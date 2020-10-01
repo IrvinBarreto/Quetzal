3200c595da8ca1a94d356a22ae74cc303cfd2e69
Irvin barreto <irvinbarreto07@gmail.com>
2020-10-01 15:30:08 -0500

    se agregaron todos los productos a la base de datos quetzal

--- app.js
+++ app.js
@@ -23,15 +23,21 @@ const quetzalProductoSchema = new mongoose.Schema({
 
 const Producto = mongoose.model("producto",quetzalProductoSchema);
 
-const ajoNegro = new Producto({
-    name:"Ajo negro",
-    url: "/img/ajoNegro.jpeg",
-    ingredients: "Ajo negro, moringa, espino blanco, olivo, jengibre, guanábana y ortiga.",
-    description:"Antioxidante Fortalece el sistema inmune. Mejora la circulación. Antimicrobiano, aumenta las defensas del cuerpo. Ayuda a mejorar los problemas digestivos. Mejora las vías respiratorias y ayuda cuándo existen problemas de gripe, asma, bronquitis, etc. Energizante. Previene y combate la anemia. Contiene vitamina C, B1, B2, B3, calcio, potasio y fósforo. Acelera el metabolismo. Antiestres. Disminuye el insomnio. Hidrata el organismo. Disminuye la presión arterial. Previene cáncer de Colón. Útil para las hemorragias internas y externas, por ejemplo las nasales. Previene la osteoporosis. Alivia el dolor a causa del reumatismo. Calma los dolores menstruales. Previene la diabetes Combate la migraña Ayuda en el tratamiento de gastritis y úlcera Aumenta el libido."
+/* const colageinX = new Producto({
+    name:"COLAGEIN-X",
+    url: "/img/colageinX.jpeg",
+    ingredients: "Arándano, Granada.",
+    description:"Regenerador celular para cabello, le da fuerza, brillo y volumen. Endurece uñas. Fortalece la piel, la reafirma y suaviza. Previene enfermedades cardiovasculares. Desvanece líneas de expresión, manchas por el sol y paño. Quita celulitis, ayuda a mantener la elasticidad de la piel. Ayuda a prevenir la metástasis (proceso cuándo una célula se convierte en cáncer) Auxiliar en artritis y osteoporosis. Fortalece y regenera el cartílago."
     
 });
 
-/* ajoNegro.save(); */
+colageinX.save(function(err){
+    if(!err){
+        console.log("guardado correctamente")
+       
+    }
+}); */
+
 
 
 
